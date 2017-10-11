155be0a3336f96e23efd865bf803b79ef99acf5b
ffrachia <ffrachia@isaltda.com.uy>
2017-10-11 18:19:42 -0300

    primer commit

---
+++ ObligatorioA2/src/dominio/Sistema.java
@@ -0,0 +1,133 @@
+package dominio;
+import dominio.Retorno.Resultado;
+
+public class Sistema implements ISistema {
+
+	@Override
+	public Retorno inicializarSistema(int cantPuntos) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno destruirSistema() {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno registrarProductor(String cedula, String nombre, String direccion, String email, String celular) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno registrarCiudad(String nombre, Double coordX, Double coordY) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno registrarPlantación(String nombre, Double coordX, Double coordY, String cedula_productor,
+			int capacidad) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno registrarSilo(String nombre, Double coordX, Double coordY, int capacidad) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno registrarTramo(Double coordXi, Double coordYi, Double coordXf, Double coordYf, int peso) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno eliminarTramo(Double coordXi, Double coordYi, Double coordXf, Double coordYf) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno eliminarPunto(Double coordX, Double coordY) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno mapaEstado() {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno rutaASiloMasCercano(Double coordX, Double coordY) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno listadoDePlantacionesEnCiudad(Double coordX, Double coordY) {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno listadoDeSilos() {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+	@Override
+	public Retorno listadoProductores() {
+		Retorno ret = new Retorno();
+		
+		ret.resultado = Resultado.NO_IMPLEMENTADA;
+		
+		return ret;
+	}
+
+}
