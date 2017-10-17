61076405b9478a605674420b6e1d7241f9b43152
Tincho <mduran@isaltda.com.uy>
2017-10-17 12:30:08 -0300

    funciones auxiliares

--- ObligatorioA2/src/funcionesAuxiliares/FuncionesAux.java
+++ ObligatorioA2/src/funcionesAuxiliares/FuncionesAux.java
@@ -1,16 +1,34 @@
 package funcionesAuxiliares;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FuncionesAux {
 	
+	
+	private Pattern pattern;
+	private Matcher matcher;
+
+	private static final String EMAIL_PATTERN =
+		"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	
+	
+	public FuncionesAux() {
+		
+	}
+	
+	public boolean validarEmail(String email) {
+		pattern = Pattern.compile(EMAIL_PATTERN);
+		
+		matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
 	
 	public boolean validarCedula(String ci) {
 		
 		return false;
 	}
-	
-	public boolean validarEmail(String email) {
-		
-		return false;
-	}
+
 	
 }
