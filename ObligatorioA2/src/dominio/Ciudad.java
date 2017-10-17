b318e4d8395037e33fd294d5317d1c11b64564bf
Tincho <mduran@isaltda.com.uy>
2017-10-17 12:20:14 -0300

    Tincho

--- ObligatorioA2/src/dominio/Ciudad.java
+++ ObligatorioA2/src/dominio/Ciudad.java
@@ -2,4 +2,41 @@ package dominio;
 
 public class Ciudad {
 
+	String nombre;
+	double coordX;
+	double coordY;
+	
+	
+	public Ciudad(String nombre, Double coordX, Double coordY) {
+		this.nombre=nombre;
+		this.coordX=coordX;
+		this.coordY=coordY;
+	}
+	
+	public Ciudad() {
+		
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getCoordX() {
+		return coordX;
+	}
+	public void setCoordX(double coordX) {
+		this.coordX = coordX;
+	}
+	public double getCoordY() {
+		return coordY;
+	}
+	public void setCoordY(double coordY) {
+		this.coordY = coordY;
+	}
+	
+	
+	
+	
 }
